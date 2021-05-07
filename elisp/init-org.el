a395e6ac730fcd5f920b04fdd4f2893a2620df9f
50ways2sayhard <gjtzone@hotmail.com>
2021-05-07 18:57:40 +0800

    :zap: setup org

--- elisp/init-org.el
+++ elisp/init-org.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 11:09:30 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Sun Feb 23 14:51:39 2020 (+0800)
+;; Last-Updated: Fri May  7 18:55:05 2021 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d org toc-org htmlize ox-gfm
@@ -36,79 +36,246 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
+(require 'org/+tables)
+(require 'org/+funcs)
 
 ;; OrgPac
+(defvar +org-capture-file-gtd (concat org-base-dir "gtd.org"))
+(defvar +org-capture-file-idea (concat org-base-dir "ideas.org"))
+(defvar +org-capture-file-note (concat org-base-dir "notes.org"))
+(defvar +org-capture-file-inbox (concat org-base-dir "inbox.org"))
+(defvar +org-capture-file-someday (concat org-base-dir "someday.org"))
+(defvar +org-capture-file-tickler (concat org-base-dir "tickler.org"))
+(defun archive-done-tasks ()
+  (interactive)
+  (save-excursion
+    (goto-char (point-min))
+    (while (re-search-forward
+            (concat "\\* " (regexp-opt org-done-keywords) " ") nil t)
+      (goto-char (line-beginning-position))
+      (org-archive-subtree))))
 (use-package org
   :ensure nil
-  :defer t
-  :bind
-  ("C-c l" . org-store-link)
-  ("C-c a" . org-agenda)
-  ("C-c c" . org-capture)
-  ("C-c b" . org-switch)
-  (:map org-mode-map ("C-c C-p" . org-export-as-pdf-and-open))
+  :hook (org-mode . org-indent-mode)
   :custom
   (org-log-done 'time)
   (org-export-backends (quote (ascii html icalendar latex md odt)))
   (org-use-speed-commands t)
   (org-confirm-babel-evaluate 'nil)
-  (org-todo-keywords
-   '((sequence "TODO" "IN-PROGRESS" "REVIEW" "|" "DONE")))
-  (org-agenda-window-setup 'other-window)
-  (org-directory (expand-file-name "~/Dropbox/org-notes"))
+  (org-directory (expand-file-name org-base-dir))
+  (org-ellipsis " ▼ ")
+  (org-babel-python-command "python3")
+  (org-bullets-bullet-list '("#"))
+  (org-tags-column -77)
+
   :config
-  (unless (version< org-version "9.2")
-    (require 'org-tempo))
-  (when (file-directory-p org-directory)
-    (setq org-agenda-files (list org-directory)))
+  (add-hook (quote hack-local-variables-hook)
+            (lambda ()
+              (let ((symbol (quote org-startup-folded)))
+                (when (and (eq major-mode (quote org-mode))
+                           (boundp symbol))
+                  (let ((value (symbol-value symbol)))
+                    (when (and value (integerp value))
+                      (org-shifttab value)))))))
+  (setq org-format-latex-options (plist-put org-format-latex-options :scale 2.0))
 
-  (defun org-export-turn-on-syntax-highlight ()
-    "Setup variables to turn on syntax highlighting when calling `org-latex-export-to-pdf'."
-    (interactive)
-    (setq org-latex-listings 'minted
-          org-latex-packages-alist '(("" "minted"))
-          org-latex-pdf-process
-          '("pdflatex -shelnl-escape -interaction nonstopmode -output-directory %o %f"
-            "pdflatex -shell-escape -interaction nonstopmode -output-directory %o %f")))
+  (+org-init-appearance-h)
+  (+org-init-agenda-h)
+  (+org-init-capture-defaults-h)
 
-  (defun org-export-as-pdf-and-open ()
-    "Run `org-latex-export-to-pdf', delete the tex file and open pdf in a new buffer."
-    (interactive)
-    (save-buffer)
-    (let* ((pdf-path (org-latex-export-to-pdf))
-           (pdf-name (file-name-nondirectory pdf-path)))
-      (if (try-completion pdf-name (mapcar #'buffer-name (buffer-list)))
-          (progn
-            (kill-matching-buffers (concat "^" pdf-name) t t)
-            (find-file-other-window pdf-name))
-        (find-file-other-window pdf-name))
-      (delete-file (concat (substring pdf-path 0 (string-match "[^\.]*\/?$" pdf-path)) "tex")))))
+  ;; org screenshot for macos
+  (require 'org/+screenshot)
+
+  (setq org-log-into-drawer "LOGBOOK")
+  (setq org-agenda-files (list +org-capture-file-inbox
+                               +org-capture-file-gtd
+                               +org-capture-file-tickler))
+  (setq org-refile-targets '((+org-capture-file-gtd :level . 1)
+                             (+org-capture-file-someday :level . 1)
+                             (+org-capture-file-tickler :level . 1)))
+  (setq org-log-into-drawer t)
+  (setq org-tag-alist '(("lab" . ?L) ("academic" . ?a) ("life" . ?l) ("paper" . ?p) ("emacs" . ?e)))
+  (setq org-capture-templates
+        '(("t" "Todo [inbox]" entry
+           (file+headline +org-capture-file-inbox "Tasks")
+           "* TODO %?\n%i" :prepend t :kill-buffer t)
+          ("w" "Waiting for" entry
+           (file+headline +org-capture-file-tickler "Tickler")
+           "* %?\n%i" :prepend t :kill-buffer t)
+          ("n" "Note" entry
+           (file+headline +org-capture-file-note "Notes")
+           "* %u %?\n%i" :prepend t :kill-buffer t)
+          ("i" "Idea" entry
+           (file+headline +org-capture-file-idea "Ideas")
+           "* %u %?\n%i" :prepend t :kill-buffer t)
+          )
+        org-todo-keywords
+        '((sequence "TODO(t!)" "|" "DONE(d!)" "CANCELLED(c!)"))
+        ;; org-agenda-window-setup 'other-window
+        )
+
+  (add-hook 'org-mode-hook #'+org-enable-auto-reformat-tables-h)
+  ;; TocOrgPac
+  (use-package toc-org
+    :after org
+    :hook (org-mode . toc-org-enable)
+    :config (setq toc-org-hrefify-default "gh")
+    )
+  ;; -TocOrgPac
+
+
+  ;; binding
+  (evil-set-initial-state 'org-agenda-mode 'motion)
+  (general-define-key :states '(normal insert)
+                      :keymaps 'org-mode-map
+                      "C-<ret>" #'+org/insert-item-below
+                      "C-S-<ret>" #'+org/insert-item-above
+                      )
+
+  (local-leader-def
+    :keymaps 'org-mode-map
+    "'" 'org-edit-special
+    "*" 'org-ctrl-c-star
+    "+" 'org-ctrl-c-minus
+    "," 'org-switchb
+    ;; "." 'org-goto
+
+    "." 'counsel-org-goto
+    "/" 'counsel-org-goto-all
+
+    "A" 'org-archive-subtree
+    "e" 'org-export-dispatch
+    "f" 'org-footnote-new
+    "h" 'org-toggle-heading
+    "i" 'org-toggle-item
+    "I" 'org-toggle-inline-images
+    "n" 'org-store-link
+    "o" 'org-set-property
+    "q" 'org-set-tags-command
+    "t" 'org-todo
+    "T" 'org-todo-list
+    "x" 'org-toggle-checkbox
+
+    "a" '(:wk "attackments")
+    "aa" 'org-attach
+    "ad" 'org-attach-delete-one
+    "aD" 'org-attach-delete-all
+    "af" '+org/find-file-in-attachments
+    "al" '+org/attach-file-and-insert-link
+    "an" 'org-attach-new
+    "ao" 'org-attach-open
+    "aO" 'org-attach-open-in-emacs
+    "ar" 'org-attach-reveal
+    "aR" 'org-attach-reveal-in-emacs
+    "au" 'org-attach-url
+    "as" 'org-attach-set-directory
+    "aS" 'org-attach-sync
+
+    "b"  '(:wk "tables")
+    "b-" 'org-table-insert-hline
+    "ba" 'org-table-align
+    "bb" 'org-table-blank-field
+    "bc" 'org-table-create-or-convert-from-region
+    "be" 'org-table-edit-field
+    "bf" 'org-table-edit-formulas
+    "bh" 'org-table-field-info
+    "bs" 'org-table-sort-lines
+    "br" 'org-table-recalculate
+    "bR" 'org-table-recalculate-buffer-tables
+    "bd" '(:wk "delete")
+    "bdc" 'org-table-delete-column
+    "bdr" 'org-table-kill-row
+    "bi" '(:wk "insert")
+    "bic" 'org-table-insert-column
+    "bih" 'org-table-insert-hline
+    "bir" 'org-table-insert-row
+    "biH" 'org-table-hline-and-move
+    "bt" '("toggle")
+    "btf" 'org-table-toggle-formula-debugger
+    "bto" 'org-table-toggle-coordinate-overlays
+
+    "c" '(:wk "clock")
+    "cc" 'org-clock-cancel
+    "cd" 'org-clock-mark-default-task
+    "ce" 'org-clock-modify-effort-estimate
+    "cE" 'org-set-effort
+    "cg" 'org-clock-goto
+    "cl" '+org/toggle-last-clock
+    "ci" 'org-clock-in
+    "cI" 'org-clock-in-last
+    "co" 'org-clock-out
+    "cr" 'org-resolve-clocks
+    "cR" 'org-clock-report
+    "ct" 'org-evaluate-time-range
+    "c=" 'org-clock-timestamps-up
+    "c-" 'org-clock-timestamps-down
+
+    "d" '(:wk "date/deadline")
+    "dd" 'org-deadline
+    "ds" 'org-schedule
+    "dt" 'org-time-stamp
+    "dT" 'org-time-stamp-inactive
+
+    "D" 'archive-done-tasks
+
+    "g" '(:wk "goto")
+    "gg" 'counsel-org-goto
+    "gG" 'counsel-org-goto-all
+    "gc" 'org-clock-goto
+    "gi" 'org-id-goto
+    "gr" 'org-refile-goto-last-stored
+    "gv" '+org/goto-visible
+    "gx" 'org-capture-goto-last-stored
+
+    "l" '(:wk "links")
+    "lc" 'org-cliplink
+    "ld" '+org/remove-link
+    "li" 'org-id-store-link
+    "ll" 'org-insert-link
+    "lL" 'org-insert-all-links
+    "ls" 'org-store-link
+    "lS" 'org-insert-last-stored-link
+    "lt" 'org-toggle-link-display
+
+    "P" '(:wk "publish")
+    "Pa" 'org-publish-all
+    "Pf" 'org-publish-current-file
+    "Pp" 'org-publish
+    "PP" 'org-publish-current-project
+    "Ps" 'org-publish-sitemap
+
+    "r" '(:wk "refile")
+    "r." '+org/refile-to-current-file
+    "rc" '+org/refile-to-running-clock
+    "rl" '+org/refile-to-last-location
+    "rf" '+org/refile-to-file
+    "ro" '+org/refile-to-other-window
+    "rO" '+org/refile-to-other-buffer
+    "rv" '+org/refile-to-visible
+    "rr" 'org-refile
+
+    "s" '(:wk "tree/subtree")
+    "sa" 'org-toggle-archive-tag
+    "sb" 'org-tree-to-indirect-buffer
+    "sd" 'org-cut-subtree
+    "sh" 'org-promote-subtree
+    "sj" 'org-move-subtree-down
+    "sk" 'org-move-subtree-up
+    "sl" 'org-demote-subtree
+    "sn" 'org-narrow-to-subtree
+    "sr" 'org-refile
+    "ss" 'org-sparse-tree
+    "sA" 'org-archive-subtree
+    "sN" 'widen
+    "sS" 'org-sort
+
+    "p" '(:wk "priority")
+    "pd" 'org-priority-down
+    "pp" 'org-priority
+    "pu" 'org-priority-up
+    )
+  )
 ;; -OrgPac
-
-;; TocOrgPac
-(use-package toc-org
-  :hook (org-mode . toc-org-mode))
-;; -TocOrgPac
-
-;; HTMLIZEPac
-(use-package htmlize :defer t)
-;; -HTMLIZEPac
-
-;; OXGFMPac
-(use-package ox-gfm :defer t)
-;; -OXGFMPac
-
-;; PlantUMLPac
-(use-package plantuml-mode
-  :defer t
-  :custom
-  (org-plantuml-jar-path (expand-file-name "~/tools/plantuml/plantuml.jar"))
-  :config
-  (org-babel-do-load-languages
-   'org-babel-load-languages
-   '(;; other Babel languages
-     (plantuml . t))))
-;; -PlantUMLPac
 
 (provide 'init-org)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
