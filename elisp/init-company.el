d5fca0526ffc1181c2abb00c885ed7a898343ff4
50ways2sayhard <gjtzone@hotmail.com>
2021-06-17 22:00:03 +0800

    :zap: speed up completing in lisp-mode

--- elisp/init-company.el
+++ elisp/init-company.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 10:02:00 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Mon May 10 14:31:53 2021 (+0800)
+;; Last-Updated: Thu Jun 17 21:53:31 2021 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d company company-tabnine
@@ -123,6 +123,7 @@ Examples:
          ("<tab>" . company-complete-common-or-cycle)
          ("TAB" . company-complete-common-or-cycle)
          ("<backtab>" . my-company-yasnippet)
+         ("<C-RET>" . company-complete)
          :map company-search-map
          ("C-p" . company-select-previous)
          ("C-n" . company-select-next))
@@ -158,14 +159,26 @@ If failed try to complete the common part with `company-complete-common'"
             (when (and (eq old-point (point))
                        (eq old-tick (buffer-chars-modified-tick)))
               (company-complete-common))))
-      (company-complete-common))))
-;; -ComPac
+      (company-complete-common)))
 
-;; CompanyLSPPac
-;; (use-package company-lsp
-;;   :defer t
-;;   :custom (company-lsp-cache-candidates 'auto))
-;; -CompanyLSPPac
+  ;; do not use orderless when completing
+  (with-eval-after-load 'orderless
+    (defvar-local +company-completion-styles '(partial-completion))
+    (defvar-local +completion-styles nil)
+    (defun set-company-completion-style (backend)
+      (setq +completion-styles completion-styles)
+      (setq completion-styles +company-completion-styles))
+    (defun restore-company-completion-style ()
+      (when +completion-styles
+        (setq completion-styles +completion-styles)
+        (setq +completion-styles nil)))
+
+    (add-hook 'company-completion-started-hook #'set-company-completion-style)
+    ;; (add-hook 'company-completion-cancelled-hook #'restore-company-completion-style)
+    ;; (add-hook 'company-completion-finished-hook #'restore-company-completion-style)
+    (add-hook 'evil-normal-state-entry-hook #'restore-company-completion-style))
+  )
+;; -ComPac
 
 (use-package company-prescient
   :init (company-prescient-mode 1))
@@ -208,70 +221,15 @@ If failed try to complete the common part with `company-complete-common'"
                )))))
 ;; -Companytabninepac
 
-(use-package company-box
-  :diminish
-  :defines company-box-icons-all-the-icons
-  :hook (company-mode . company-box-mode)
-  :if (display-graphic-p)
-  :init
-  (setq company-box-backends-colors nil
-        company-box-doc-delay 0.3)
-  :config
-  (with-no-warnings
-    ;; Prettify icons
-    (defun my-company-box-icons--elisp (candidate)
-      (when (derived-mode-p 'emacs-lisp-mode)
-        (let ((sym (intern candidate)))
-          (cond ((fboundp sym) 'Function)
-                ((featurep sym) 'Module)
-                ((facep sym) 'Color)
-                ((boundp sym) 'Variable)
-                ((symbolp sym) 'Text)
-                (t . nil)))))
-    (advice-add #'company-box-icons--elisp :override #'my-company-box-icons--elisp))
-
-  (when (icons-displayable-p)
-    (declare-function all-the-icons-faicon 'all-the-icons)
-    (declare-function all-the-icons-material 'all-the-icons)
-    (declare-function all-the-icons-octicon 'all-the-icons)
-    (setq company-box-icons-all-the-icons
-          `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.8 :v-adjust -0.15))
-            (Text . ,(all-the-icons-faicon "text-width" :height 0.8 :v-adjust -0.02))
-            (Method . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Function . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Constructor . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Field . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-            (Variable . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-            (Class . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Interface . ,(all-the-icons-material "share" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Module . ,(all-the-icons-material "view_module" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Property . ,(all-the-icons-faicon "wrench" :height 0.8 :v-adjust -0.02))
-            (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.8 :v-adjust -0.15))
-            (Value . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Enum . ,(all-the-icons-material "storage" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.8 :v-adjust -0.15))
-            (Snippet . ,(all-the-icons-material "format_align_center" :height 0.8 :v-adjust -0.15))
-            (Color . ,(all-the-icons-material "palette" :height 0.8 :v-adjust -0.15))
-            (File . ,(all-the-icons-faicon "file-o" :height 0.8 :v-adjust -0.02))
-            (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.8 :v-adjust -0.15))
-            (Folder . ,(all-the-icons-faicon "folder-open" :height 0.8 :v-adjust -0.02))
-            (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15))
-            (Constant . ,(all-the-icons-faicon "square-o" :height 0.8 :v-adjust -0.1))
-            (Struct . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Event . ,(all-the-icons-octicon "zap" :height 0.8 :v-adjust 0 :face 'all-the-icons-orange))
-            (Operator . ,(all-the-icons-material "control_point" :height 0.8 :v-adjust -0.15))
-            (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.8 :v-adjust -0.02))
-            (Template . ,(all-the-icons-material "format_align_left" :height 0.8 :v-adjust -0.15)))
-          company-box-icons-alist 'company-box-icons-all-the-icons))
-  )
-
 (use-package company-quickhelp
   :defines company-quickhelp-delay
   :if (display-graphic-p)
   :bind (:map company-active-map
-              ([remap company-show-doc-buffer] . company-quickhelp-manual-begin))
-  ;; :hook (global-company-mode . company-quickhelp-mode)
+              ([remap company-show-doc-buffer] . company-quickhelp-manual-begin)
+              ("C-c d" . company-quickhelp-manual-begin))
+  :hook (global-company-mode . company-quickhelp-mode)
   :init (setq company-quickhelp-delay 0.5))
+
 
 
 (provide 'init-company)
