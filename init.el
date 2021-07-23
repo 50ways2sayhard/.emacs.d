87ff7a33f243f2efb954a23638dd9caf3e3ea557
50ways2sayhard <gjtzone@hotmail.com>
2021-07-24 03:09:14 +0800

    Revert ":fire: remove `shackle`"
    This reverts commit 0c93fd14f41e84f00d085aa5a45f5e743bbcd343.

--- init.el
+++ init.el
@@ -171,6 +171,7 @@
 (require 'init-bindings)
 (require 'init-org)
 (require 'init-restart-emacs)
+(require 'init-shackle)
 
 (provide 'init)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
