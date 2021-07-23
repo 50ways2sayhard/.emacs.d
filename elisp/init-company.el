2d938c7ad3ae9ba37e1211cc0265dcd17d2d26b9
50ways2sayhard <gjtzone@hotmail.com>
2021-07-24 02:36:29 +0800

    :recycle: [company] refactor

--- elisp/init-company.el
+++ elisp/init-company.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 10:02:00 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Sun Jul 11 14:26:56 2021 (+0800)
+;; Last-Updated: Sat Jul 24 01:44:29 2021 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d company company-tabnine
@@ -36,14 +36,48 @@
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 ;;
 ;;; Code:
-
 (eval-when-compile
-  (require 'init-const))
+  (require 'init-const)
+  (require 'init-bindings))
+
+
+(defun smarter-yas-expand-next-field-complete ()
+  "Try to `yas-expand' and `yas-next-field' at current cursor position.
+If failed try to complete the common part with `company-complete-common'"
+  (interactive)
+  (if yas-minor-mode
+      (let ((old-point (point))
+            (old-tick (buffer-chars-modified-tick)))
+        (yas-expand)
+        (when (and (eq old-point (point))
+                   (eq old-tick (buffer-chars-modified-tick)))
+          (ignore-errors (yas-next-field))
+          (when (and (eq old-point (point))
+                     (eq old-tick (buffer-chars-modified-tick)))
+            (progn
+              (call-interactively 'company-abort)
+              (call-interactively 'company-yasnippet))
+            )
+          ))
+    ;; FIXME: c-k tab c-k
+    (company-complete-common)
+    )
+  )
+
+(defun company-yasnippet-unless-member-access (command &optional arg &rest ignore)
+  (if (eq command 'prefix)
+      (let ((prefix (company-yasnippet 'prefix)))
+        (and prefix
+             (save-excursion
+               (forward-char (- (length prefix)))
+               (not (looking-back (rx (or "." "->")) (line-beginning-position))))
+             prefix))
+    (company-yasnippet command arg)))
 
 ;;;###autoload
 (defvar +company-backend-alist
   '((text-mode company-tabnine company-yasnippet company-dabbrev)
-    (prog-mode company-files company-capf company-yasnippet)
+    (prog-mode company-files (company-capf :with company-tabnine :with company-yasnippet-unless-member-access :separate))
     (conf-mode company-capf company-dabbrev-code company-yasnippet))
   "An alist matching modes to company backends. The backends for any mode is
 built from this.")
@@ -108,61 +142,42 @@ Examples:
 
 (put '+company-init-backends-h 'permanent-local-hook t)
 
-
 ;; ComPac
 (use-package company
-  :diminish "C"
-  :defines (company-dabbrev-ignore-case company-dabbrev-downcase)
-  :commands company-abort
-  :hook (after-init . global-company-mode)
-  :bind (("M-/" . company-complete)
-         ("C-M-i" . company-complete)
-         :map company-active-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next)
-         ("<tab>" . company-complete-common-or-cycle)
-         ("TAB" . company-complete-common-or-cycle)
-         ("<backtab>" . my-company-yasnippet)
-         ("<C-RET>" . company-complete)
-         :map company-search-map
-         ("C-p" . company-select-previous)
-         ("C-n" . company-select-next))
-  :config
-  (setq company-tooltip-align-annotations t
-        company-tooltip-limit 12
-        company-show-numbers t
-        company-idle-delay 0
-        company-files-chop-trailing-slash t
-        company-echo-delay (if (display-graphic-p) nil 0)
-        company-minimum-prefix-length 1
-        company-require-match 'never
-        company-dabbrev-ignore-case nil
-        company-dabbrev-downcase nil
-        company-backends '(company-files company-capf)
-        company-global-modes '(not erc-mode message-mode help-mode gud-mode eshell-mode shell-mode)
-        )
+  :diminish company-mode
+  :hook ((prog-mode LaTeX-mode) . company-mode)
+  :init
+  (company-tng-mode)
   (add-hook 'company-mode-hook #'+company-init-backends-h)
-  (add-to-list 'company-transformers #'delete-dups)
+  :custom
+  (company-files-chop-trailing-slash nil)
+  (company-minimum-prefix-length 1)
+  (company-tooltip-align-annotations t)
+  (company-require-match 'never)
+  ;; Don't use company in the following modes
+  (company-global-modes '(not shell-mode))
+  ;; Trigger completion immediately.
+  (company-idle-delay 0)
+  ;; Number the candidates (use M-1, M-2 etc to select completions).
+  (company-show-numbers nil)
+  ;; (company-tooltip-minimum-width 80)
+  (company-quickelp-delay nil)
+  (company-dabbrev-downcase nil)
+  (company-dabbrev-ignore-case t)
+  (company-frontends '(company-pseudo-tooltip-unless-just-one-frontend
+                       company-preview-frontend
+                       company-echo-metadata-frontend))
+  :config
+  (setq company-backends '(company-files company-dabbrev))
   (global-company-mode 1)
-  (company-tng-mode 1)
-  (defun smarter-yas-expand-next-field-complete ()
-    "Try to `yas-expand' and `yas-next-field' at current cursor position.
 
-If failed try to complete the common part with `company-complete-common'"
-    (interactive)
-    (if yas-minor-mode
-        (let ((old-point (point))
-              (old-tick (buffer-chars-modified-tick)))
-          (yas-expand)
-          (when (and (eq old-point (point))
-                     (eq old-tick (buffer-chars-modified-tick)))
-            (ignore-errors (yas-next-field))
-            (when (and (eq old-point (point))
-                       (eq old-tick (buffer-chars-modified-tick)))
-              (company-complete-common))))
-      (company-complete-common)))
+  (general-define-key
+   :keymaps '(company-active-map evil-insert-state-map)
+   "C-k" 'smarter-yas-expand-next-field-complete)
+  (general-def 'insert
+    :prefix "C-x"
+    "C-f" 'company-files)
 
-  ;; do not use orderless when completing
   (with-eval-after-load 'orderless
     (defvar-local +company-completion-styles '(partial-completion))
     (defvar-local +completion-styles nil)
@@ -175,15 +190,23 @@ If failed try to complete the common part with `company-complete-common'"
         (setq +completion-styles nil)))
 
     (add-hook 'company-completion-started-hook #'set-company-completion-style)
+    ;; (add-hook 'company-completion-cancelled-hook #'restore-company-completion-style)
+    ;; (add-hook 'company-completion-finished-hook #'restore-company-completion-style)
     (add-hook 'evil-normal-state-entry-hook #'restore-company-completion-style))
   )
 ;; -ComPac
-
 (use-package company-prescient
   :init (company-prescient-mode 1))
 
+
+(use-package company-box
+  :after company
+  :hook (company-mode . company-box-mode)
+  :custom
+  (company-box-max-candidates 10)
+  (company-box-doc-delay 0.3))
+
 ;; CompanyTabNinePac
-;; TODO: whether or not to use tabnine for lsp or even prog-mode.
 (use-package company-tabnine
   :straight (:host github :repo "theFool32/company-tabnine" :depth 1)
   :defer 1
@@ -192,9 +215,12 @@ If failed try to complete the common part with `company-complete-common'"
   (company-tabnine-max-num-results 3)
   :hook
   (kill-emacs . company-tabnine-kill-process)
+  :bind
+  (:map company-mode-map
+        ("C-x C-t" . company-tabnine))
   :init
-  ;; Integrate company-tabnine with lsp-mode
   (defun company//sort-by-tabnine (candidates)
+    "Integrate company-tabnine with lsp-mode"
     (if (or (functionp company-backend)
             (not (and (listp company-backend) (memq 'company-tabnine company-backend))))
         candidates
@@ -213,164 +239,13 @@ If failed try to complete the common part with `company-complete-common'"
         (nconc (seq-take candidates-lsp 2)
                (seq-take candidates-tabnine 2)
                (seq-drop candidates-lsp 2)
-               (seq-drop candidates-tabnine 2)))))
-
-  (defun company-tabnine-toggle (&optional enable)
-    "Enable/Disable TabNine. If ENABLE is non-nil, definitely enable it."
-    (interactive)
-    (if (or enable (not (memq 'company-tabnine company-backends)))
-        (progn
-          (add-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
-          (add-to-list 'company-backends #'company-tabnine)
-          (when (bound-and-true-p lsp-mode) (lsp-after-open-tabnine))
-          (message "TabNine enabled."))
-      (setq company-backends (delete 'company-tabnine company-backends))
-      (setq company-backends (delete '(company-capf :with company-tabnine :separate) company-backends))
-      (remove-hook 'lsp-after-open-hook #'lsp-after-open-tabnine)
-      (company-tabnine-kill-process)
-      (message "TabNine disabled.")))
-
-  (defun lsp-after-open-tabnine ()
-    "Hook to attach to `lsp-after-open'."
-    (setq-local company-tabnine-max-num-results 3)
-    (add-to-list 'company-transformers 'company//sort-by-tabnine t)
-    (add-to-list 'company-backends '(company-capf :with company-tabnine :separate))
-    )
+               (seq-drop candidates-tabnine 2)
+               ))))
 
   :config
-  (company-tabnine-toggle t)
+  (add-to-list 'company-transformers 'company//sort-by-tabnine t)
   )
-;; -Companytabninepac
-
-(use-package company-box
-  :if *sys/mac*
-  :diminish
-  :defines company-box-icons-all-the-icons
-  :hook (company-mode . company-box-mode)
-  :init (setq company-box-enable-icon t
-              company-box-backends-colors nil
-              company-box-doc-delay 0.3)
-  :config
-  (with-no-warnings
-    ;; Prettify icons
-    (defun my-company-box-icons--elisp (candidate)
-      (when (or (derived-mode-p 'emacs-lisp-mode) (derived-mode-p 'lisp-mode))
-        (let ((sym (intern candidate)))
-          (cond ((fboundp sym) 'Function)
-                ((featurep sym) 'Module)
-                ((facep sym) 'Color)
-                ((boundp sym) 'Variable)
-                ((symbolp sym) 'Text)
-                (t . nil)))))
-    (advice-add #'company-box-icons--elisp :override #'my-company-box-icons--elisp)
-
-    ;; Display borders of child frame
-    (setq company-box-doc-frame-parameters '((internal-border-width . 1)
-                                             (left-fringe . 10)
-                                             (right-fringe . 10)))
-    (defun my-company-box-doc--make-buffer (object)
-      (let* ((buffer-list-update-hook nil)
-             (inhibit-modification-hooks t)
-             (string (cond ((stringp object) object)
-                           ((bufferp object) (with-current-buffer object (buffer-string))))))
-        (when (and string (> (length (string-trim string)) 0))
-          (with-current-buffer (company-box--get-buffer "doc")
-            (erase-buffer)
-            (insert (propertize "\n" 'face '(:height 0.5)))
-            (insert string)
-            (insert (propertize "\n\n" 'face '(:height 0.5)))
-            (setq mode-line-format nil
-                  display-line-numbers nil
-                  header-line-format nil
-                  show-trailing-whitespace nil
-                  cursor-in-non-selected-windows nil)
-            (current-buffer)))))
-    (advice-add #'company-box-doc--make-buffer :override #'my-company-box-doc--make-buffer)
-
-    (defun my-company-box-doc--show (selection frame)
-      (cl-letf (((symbol-function 'completing-read) #'company-box-completing-read)
-                (window-configuration-change-hook nil)
-                (inhibit-redisplay t)
-                (display-buffer-alist nil)
-                (buffer-list-update-hook nil))
-        (-when-let* ((valid-state (and (eq (selected-frame) frame)
-                                       company-box--bottom
-                                       company-selection
-                                       (company-box--get-frame)
-                                       (frame-visible-p (company-box--get-frame))))
-                     (candidate (nth selection company-candidates))
-                     (doc (or (company-call-backend 'quickhelp-string candidate)
-                              (company-box-doc--fetch-doc-buffer candidate)))
-                     (doc (company-box-doc--make-buffer doc)))
-          (let ((frame (frame-local-getq company-box-doc-frame))
-                (border-color (face-foreground 'font-lock-comment-face nil t)))
-            (unless (frame-live-p frame)
-              (setq frame (company-box-doc--make-frame doc))
-              (frame-local-setq company-box-doc-frame frame))
-            (set-face-background 'internal-border border-color frame)
-            (when (facep 'child-frame-border)
-              (set-face-background 'child-frame-border border-color frame))
-            (company-box-doc--set-frame-position frame)
-            (unless (frame-visible-p frame)
-              (make-frame-visible frame))))))
-    (advice-add #'company-box-doc--show :override #'my-company-box-doc--show)
-
-    (defun my-company-box--display (string on-update)
-      "Display the completions."
-      (company-box--render-buffer string on-update)
-
-      (let ((frame (company-box--get-frame))
-            (border-color (face-foreground 'font-lock-comment-face nil t)))
-        (unless frame
-          (setq frame (company-box--make-frame))
-          (company-box--set-frame frame))
-        (company-box--compute-frame-position frame)
-        (company-box--move-selection t)
-        (company-box--update-frame-position frame)
-        (unless (frame-visible-p frame)
-          (make-frame-visible frame))
-        (company-box--update-scrollbar frame t)
-        (set-face-background 'internal-border border-color frame)
-        (when (facep 'child-frame-border)
-          (set-face-background 'child-frame-border border-color frame)))
-      (with-current-buffer (company-box--get-buffer)
-        (company-box--maybe-move-number (or company-box--last-start 1))))
-    (advice-add #'company-box--display :override #'my-company-box--display))
-
-  (when (icons-displayable-p)
-    (declare-function all-the-icons-faicon 'all-the-icons)
-    (declare-function all-the-icons-material 'all-the-icons)
-    (declare-function all-the-icons-octicon 'all-the-icons)
-    (setq company-box-icons-all-the-icons
-          `((Unknown . ,(all-the-icons-material "find_in_page" :height 0.8 :v-adjust -0.15))
-            (Text . ,(all-the-icons-faicon "text-width" :height 0.8 :v-adjust -0.02))
-            (Method . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Function . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Constructor . ,(all-the-icons-faicon "cube" :height 0.8 :v-adjust -0.02 :face 'all-the-icons-purple))
-            (Field . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-            (Variable . ,(all-the-icons-octicon "tag" :height 0.85 :v-adjust 0 :face 'all-the-icons-lblue))
-            (Class . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Interface . ,(all-the-icons-material "share" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Module . ,(all-the-icons-material "view_module" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Property . ,(all-the-icons-faicon "wrench" :height 0.8 :v-adjust -0.02))
-            (Unit . ,(all-the-icons-material "settings_system_daydream" :height 0.8 :v-adjust -0.15))
-            (Value . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-lblue))
-            (Enum . ,(all-the-icons-material "storage" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Keyword . ,(all-the-icons-material "filter_center_focus" :height 0.8 :v-adjust -0.15))
-            (Snippet . ,(all-the-icons-material "format_align_center" :height 0.8 :v-adjust -0.15))
-            (Color . ,(all-the-icons-material "palette" :height 0.8 :v-adjust -0.15))
-            (File . ,(all-the-icons-faicon "file-o" :height 0.8 :v-adjust -0.02))
-            (Reference . ,(all-the-icons-material "collections_bookmark" :height 0.8 :v-adjust -0.15))
-            (Folder . ,(all-the-icons-faicon "folder-open" :height 0.8 :v-adjust -0.02))
-            (EnumMember . ,(all-the-icons-material "format_align_right" :height 0.8 :v-adjust -0.15))
-            (Constant . ,(all-the-icons-faicon "square-o" :height 0.8 :v-adjust -0.1))
-            (Struct . ,(all-the-icons-material "settings_input_component" :height 0.8 :v-adjust -0.15 :face 'all-the-icons-orange))
-            (Event . ,(all-the-icons-octicon "zap" :height 0.8 :v-adjust 0 :face 'all-the-icons-orange))
-            (Operator . ,(all-the-icons-material "control_point" :height 0.8 :v-adjust -0.15))
-            (TypeParameter . ,(all-the-icons-faicon "arrows" :height 0.8 :v-adjust -0.02))
-            (Template . ,(all-the-icons-material "format_align_left" :height 0.8 :v-adjust -0.15)))
-          company-box-icons-alist 'company-box-icons-all-the-icons)))
-
+;; -CompanyTabNinePac
 
 
 (provide 'init-company)
