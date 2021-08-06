5183313dbdeabec99c89da55f54270cc902370fe
root <root@johngong@futunn.com>
2021-08-06 17:34:22 +0800

    :children_crossing: better scroll

--- elisp/init-scroll.el
+++ elisp/init-scroll.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 08:30:08 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Thu Jun 17 22:14:57 2021 (+0800)
+;; Last-Updated: Thu Aug  5 17:50:05 2021 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d smooth-scroll
@@ -39,8 +39,11 @@
 
 ;; SmoothScroll
 ;; Vertical Scroll
-(setq scroll-step 1)
-(setq scroll-margin 1)
+(setq scroll-step 1
+      scroll-margin 0
+      scroll-conservatively 100000
+      auto-window-vscroll nil
+      scroll-preserve-screen-position t)
 (setq scroll-conservatively 101)
 (setq scroll-up-aggressively 0.01)
 (setq scroll-down-aggressively 0.01)
@@ -53,12 +56,15 @@
 (setq hscroll-margin 1)
 ;; -SmoothScroll
 
+(setq mouse-wheel-scroll-amount '(1 ((shift) . hscroll))
+      mouse-wheel-scroll-amount-horizontal 1)
+
 (use-package good-scroll
   :diminish
-  :hook (after-init . good-scroll-mode)
+  ;; :hook (after-init . good-scroll-mode)
   :init
-  (global-set-key [next] #'good-scroll-up-full-screen)
-  (global-set-key [prior] #'good-scroll-down-full-screen)
+  ;; (global-set-key [next] #'good-scroll-up-full-screen)
+  ;; (global-set-key [prior] #'good-scroll-down-full-screen)
   )
 
 (provide 'init-scroll)
