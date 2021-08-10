88f152bf2d2e58c10979bafc357e50b31a5910d3
root <root@johngong@futunn.com>
2021-08-10 17:46:12 +0800

    :zap: increase gc threshold

--- elisp/init-global-config.el
+++ elisp/init-global-config.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Thu Mar 14 14:01:54 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Sun Aug  8 17:54:50 2021 (+0800)
+;; Last-Updated: Mon Aug  9 10:41:25 2021 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d
@@ -192,7 +192,7 @@ The original function deletes trailing whitespace of the current line."
   ;; (setq gcmh-idle-delay 5
   ;;       gcmh-high-cons-threshold #x1000000)
   (setq gcmh-idle-delay 0.5
-        gcmh-high-cons-threshold (* 16 1024 1024)))
+        gcmh-high-cons-threshold (* 64 1024 1024)))
 
 (provide 'init-global-config)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
