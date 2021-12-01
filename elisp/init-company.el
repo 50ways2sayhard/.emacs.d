9b92f91745b569f9983755a9ed90b8d3bfbc112d
50ways2sayhard <gjtzone@hotmail.com>
2021-12-01 22:34:21 +0800

    :heavy_minus_sign: remove `company-prescient`

--- elisp/init-company.el
+++ elisp/init-company.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Fri Mar 15 10:02:00 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Sat Nov 13 15:24:38 2021 (+0800)
+;; Last-Updated: Wed Dec  1 22:34:17 2021 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d company company-tabnine
@@ -210,13 +210,9 @@ Examples:
   (advice-add 'company-capf
               :around
               (lambda (capf-fn &rest args)
-                (let ((completion-styles '(basic partial-completion substring flex)))
+                (let ((completion-styles '(flex)))
                   (apply capf-fn args))))
   )
-;; -ComPac
-(use-package company-prescient
-  :after company
-  :init (company-prescient-mode 1))
 
 ;; CompanyTabNinePac
 (use-package company-tabnine
