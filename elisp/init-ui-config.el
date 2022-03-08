fdfd1f4fb0f75ec2fb9e35a8a0b15454c50f2065
50ways2sayhard <gjtzone@hotmail.com>
2022-03-08 21:10:02 +0800

    :heavy_plus_sign: add `popper`

--- elisp/init-ui-config.el
+++ elisp/init-ui-config.el
@@ -6,7 +6,7 @@
 ;; Copyright (C) 2019 Mingde (Matthew) Zeng
 ;; Created: Thu Mar 14 16:12:56 2019 (-0400)
 ;; Version: 2.0.0
-;; Last-Updated: Wed Mar  2 20:31:35 2022 (+0800)
+;; Last-Updated: Tue Mar  8 14:29:46 2022 (+0800)
 ;;           By: John
 ;; URL: https://github.com/MatthewZMD/.emacs.d
 ;; Keywords: M-EMACS .emacs.d ui
@@ -103,6 +103,83 @@
   (transient-posframe-poshandler #'posframe-poshandler-point-top-left-corner))
 
 (setq pixel-scroll-precision-mode nil)
+
+;; Enforce rules for popups
+(use-package popper
+  :defines popper-echo-dispatch-actions
+  :commands popper-group-by-projectile
+  :bind (:map popper-mode-map
+              ("C-h z" . popper-toggle-latest)
+              ("C-<tab>"   . popper-cycle)
+              ("C-M-<tab>" . popper-toggle-type))
+  :hook (after-init . popper-mode)
+  :init
+  (setq popper-reference-buffers
+        '("\\*Messages\\*"
+          "Output\\*$" "\\*Pp Eval Output\\*$"
+          "\\*Completions\\*"
+          "\\*Warnings\\*"
+          "\\*Async Shell Command\\*"
+          "\\*Apropos\\*"
+          "\\*Backtrace\\*"
+          "\\*Agenda Commands\\*"
+          "\\*eldoc\\*"
+          ;; "\\*Calendar\\*"              ; FIXME: https://github.com/karthink/popper/issues/29
+
+          bookmark-bmenu-mode
+          compilation-mode
+          help-mode helpful-mode
+          tabulated-list-mode
+          Buffer-menu-mode
+
+          gnus-article-mode devdocs-mode
+          grep-mode occur-mode rg-mode deadgrep-mode ag-mode pt-mode
+          ivy-occur-mode ivy-occur-grep-mode
+          process-menu-mode list-environment-mode cargo-process-mode
+          youdao-dictionary-mode osx-dictionary-mode fanyi-mode
+
+          "^\\*eshell.*\\*$" eshell-mode
+          "^\\*shell.*\\*$"  shell-mode
+          "^\\*term.*\\*$"   term-mode
+          "^\\*vterm.*\\*$"  vterm-mode
+
+          "\\*DAP Templates\\*$" dap-server-log-mode
+          "\\*ELP Profiling Restuls\\*" profiler-report-mode
+          "\\*Flycheck errors\\*$" " \\*Flycheck checker\\*$"
+          "\\*Paradox Report\\*$" "\\*package update results\\*$" "\\*Package-Lint\\*$"
+          "\\*[Wo]*Man.*\\*$"
+          "\\*ert\\*$" overseer-buffer-mode
+          "\\*gud-debug\\*$"
+          "\\*lsp-help\\*$" "\\*lsp session\\*$"
+          "\\*quickrun\\*$"
+          "\\*tldr\\*$"
+          "\\*vc-.*\\*$"
+          "^\\*elfeed-entry\\*$"
+          "^\\*macro expansion\\**"
+
+          "\\*Org Select\\*" "\\*Capture\\*" "^CAPTURE-.*\\.org*"
+          "\\*Gofmt Errors\\*$" "\\*Go Test\\*$" godoc-mode
+          "\\*docker-containers\\*" "\\*docker-images\\*" "\\*docker-networks\\*" "\\*docker-volumes\\*"
+          "\\*prolog\\*" inferior-python-mode inf-ruby-mode swift-repl-mode
+          "\\*rustfmt\\*$" rustic-compilation-mode rustic-cargo-clippy-mode
+          rustic-cargo-outdated-mode rustic-cargo-test-moed))
+
+  (with-eval-after-load 'projectile
+    (setq popper-group-function #'popper-group-by-projectile))
+  (setq popper-echo-dispatch-actions t)
+  :config
+  (popper-echo-mode 1)
+  (with-no-warnings
+    (defun popper-close-window-hack (&rest _)
+      "Close popper window via `C-g'."
+      ;; `C-g' can deactivate region
+      (when (and (called-interactively-p 'interactive)
+                 (not (region-active-p))
+                 popper-open-popup-alist)
+        (let ((window (caar popper-open-popup-alist)))
+          (when (window-live-p window)
+            (delete-window window)))))
+    (advice-add #'keyboard-quit :before #'popper-close-window-hack)))
 
 (provide 'init-ui-config)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
