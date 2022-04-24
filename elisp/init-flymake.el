8d539222370a2cabf4190cb1cd0b229a863da704
50ways2sayhard <gjtzone@hotmail.com>
2022-04-24 23:23:51 +0800

    [flymake] update

--- elisp/init-flymake.el
+++ elisp/init-flymake.el
@@ -10,7 +10,7 @@
 ;; Package-Requires: ()
 ;; Last-Updated:
 ;;           By:
-;;     Update #: 18
+;;     Update #: 24
 ;; URL:
 ;; Doc URL:
 ;; Keywords:
@@ -48,37 +48,49 @@
 
 ;; From purcell
 
-;; (with-eval-after-load 'flymake
-;;   (defun sanityinc/eldoc-flymake-first ()
-;;     "Gives flymake's eldoc function priority in the minibuffer."
-;;     (when flymake-mode
-;;       (setq-local eldoc-documentation-functions
-;;                   (cons 'flymake-eldoc-function
-;;                         (delq 'flymake-eldoc-function eldoc-documentation-functions)))))
+(with-eval-after-load 'flymake
+  (defun sanityinc/eldoc-flymake-first ()
+    "Gives flymake's eldoc function priority in the minibuffer."
+    (when flymake-mode
+      (setq-local eldoc-documentation-functions
+                  (cons 'flymake-eldoc-function
+                        (delq 'flymake-eldoc-function eldoc-documentation-functions)))))
 
-;;   (add-hook 'flymake-mode-hook 'sanityinc/eldoc-flymake-first))
+  (add-hook 'flymake-mode-hook 'sanityinc/eldoc-flymake-first))
 
-;; (use-package flymake-flycheck
-;;   :diminish
-;;   :config
-;;   (with-eval-after-load 'flycheck
-;;     (setq-default flycheck-disabled-checkers
-;;                   (append (default-value 'flycheck-disabled-checkers)
-;;                           '(emacs-lisp emacs-lisp-checkdoc emacs-lisp-package))))
-;;   (defun sanityinc/enable-flymake-flycheck ()
-;;     (setq-local flymake-diagnostic-functions
-;;                 (append flymake-diagnostic-functions
-;;                         (flymake-flycheck-all-chained-diagnostic-functions))))
+(setq elisp-flymake-byte-compile-load-path
+      (append elisp-flymake-byte-compile-load-path
+              load-path))
 
-;;   (add-hook 'flymake-mode-hook 'sanityinc/enable-flymake-flycheck)
-;;   (add-hook 'prog-mode-hook 'flymake-mode)
-;;   (add-hook 'text-mode-hook 'flymake-mode)
-;;   (with-eval-after-load 'general
-;;     (leader-def
-;;       :keymaps 'override
-;;       "el" '(consult-flymake :wk "List error")
-;;       "ef" '(consult-flymake :wk "Find error")))
-;;   )
+(use-package flymake-proc
+  :config
+  (progn
+    ;; flymake-proc adds this legacy backend automatically but (1) I
+    ;; don't seem to use it and (2) it triggers warnings in *Flymake
+    ;; log*.
+    (remove-hook 'flymake-diagnostic-functions #'flymake-proc-legacy-flymake)))
+
+(use-package flymake-flycheck
+  :diminish
+  :config
+  (with-eval-after-load 'flycheck
+    (setq-default flycheck-disabled-checkers
+                  (append (default-value 'flycheck-disabled-checkers)
+                          '(emacs-lisp emacs-lisp-checkdoc emacs-lisp-package))))
+  (defun sanityinc/enable-flymake-flycheck ()
+    (setq-local flymake-diagnostic-functions
+                (append flymake-diagnostic-functions
+                        (flymake-flycheck-all-chained-diagnostic-functions))))
+
+  (add-hook 'flymake-mode-hook 'sanityinc/enable-flymake-flycheck)
+  (add-hook 'prog-mode-hook 'flymake-mode)
+  (add-hook 'text-mode-hook 'flymake-mode)
+  (with-eval-after-load 'general
+    (leader-def
+      :keymaps 'override
+      "el" '(consult-flymake :wk "List error")
+      "ef" '(consult-flymake :wk "Find error")))
+  )
 
 (setq flymake-no-changes-timeout nil)
 
