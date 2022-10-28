20542d16b254f5d6398e388cdb00ecf300c369ff
johngong <johngong@futunn.com>
2022-10-28 11:20:19 +0800

    use `ef-themes`

--- elisp/init-theme.el
+++ elisp/init-theme.el
@@ -58,18 +58,24 @@
 
 ;; DoomThemes
 (use-package doom-themes
-  :custom-face
-  (cursor ((t (:background "BlanchedAlmond"))))
+  :disabled
   :config
   (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
         doom-themes-enable-italic t) ; if nil, italics is universally disabled
   ;; flashing mode-line on errors
-  ;; (doom-themes-visual-bell-config)
+  (doom-themes-visual-bell-config)
   ;; Corrects (and improves) org-mode's native fontification.
   (doom-themes-org-config)
   (load-theme 'doom-one t)
   )
 ;; -DoomThemes
+
+(use-package ef-themes
+  :straight (:repo "protesilaos/ef-themes" :host github)
+  :config
+  ;; (ef-themes-select 'ef-trio-dark)
+  (ef-themes-select 'ef-summer)
+  )
 
 (provide 'init-theme)
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
